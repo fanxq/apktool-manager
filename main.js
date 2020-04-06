dba7c9da99c4c1867dd7ee1e1c4f1b1af3c018b7
fanxq <1508422424@qq.com>
2020-04-06 11:31:35 +0800

    add method to ensure  single instance of the app

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ global.assetsPath = assetsPath;
 // 垃圾回收的时候，window对象将会自动的关闭
 let win
 let tray = null;
+const gotTheLock = app.requestSingleInstanceLock();
 
 function createWindow() {
   if (!fs.existsSync(userDataPath)) {
@@ -85,27 +86,30 @@ function createWindow() {
   })
 }
 
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow)
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (win) {
+      if (win.isMinimized()) win.restore();
+      if (!win.isVisible()) win.show();
+      win.focus()
+    }
+  });
 
-// 当全部窗口关闭时退出。
-app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  app.on('ready', createWindow);
 
-app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (win === null) {
-    createWindow()
-  }
-})
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-// 在这个文件中，你可以续写应用剩下主进程代码。
-// 也可以拆分成几个文件，然后用 require 导入。
+  app.on('activate', () => {
+    if (win === null) {
+      createWindow();
+    }
+  });
+}
+
+
